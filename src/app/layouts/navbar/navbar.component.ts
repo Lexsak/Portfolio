72d1386ccbbd3a565c34c5742033572fbda306c6
Lexsak <bartoszpiprek123@gmail.com>
2023-12-10 14:12:21 +0100

    navbar ts & html added

--- src/app/layouts/navbar/navbar.component.ts
+++ src/app/layouts/navbar/navbar.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 
+interface navLinks {
+  anchor: string;
+  text: string;
+}
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent {
-
+  navLinks: navLinks[] = [
+    { anchor: '#home', text: 'Home' },
+    { anchor: '#about', text: 'About' },
+    { anchor: '#projects', text: 'Projects' },
+    { anchor: '#contact', text: 'Contact' },
+  ];
 }
