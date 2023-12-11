77d4d3ec8306cf5636e428ea9edf9b282c365732
Lexsak <bartoszpiprek123@gmail.com>
2023-12-11 13:14:46 +0100

    skill, tools & more projects link added

--- src/app/pages/projects-section/projects-section.component.ts
+++ src/app/pages/projects-section/projects-section.component.ts
@@ -18,8 +18,7 @@ export class ProjectsSectionComponent {
   cardsProject: cardProject[] = [
     {
       title: 'ANG-BLOG',
-      desc:
-        'A blog with an admin panel where you can modify every element of the blog.',
+      desc: 'A blog with an admin panel where you can modify every element of the blog.',
       image: '../../../assets/images/ang-blog.png',
       tag: ['ANGULAR', 'FIREBASE', 'TYPESCRIPT'],
       liveLink: 'https://angular-blog-4c2c0.web.app/',
@@ -38,8 +37,25 @@ export class ProjectsSectionComponent {
       desc: 'Recipe book with login panel and fetching data.',
       image: '../../../assets/images/recipe-book.png',
       tag: ['ANGULAR', 'FIREBASE', 'TYPESCRIPT', 'bootstrap'],
-      liveLink: 'https://ng-recipe-book-e4619.web.app/auth',
+      liveLink: 'https://ng-recipe-book-e4619.web.app',
       githubLink: 'https://github.com/Lexsak/recipe-book-angular',
     },
+  ];
+
+  skills: string[] = [
+    '../../../assets/icons/html.svg',
+    '../../../assets/icons/css.svg',
+    '../../../assets/icons/js.svg',
+    '../../../assets/icons/typescript.svg',
+    '../../../assets/icons/angular.svg',
+    '../../../assets/icons/react.svg',
+    '../../../assets/icons/wordpress.svg',
+    '../../../assets/icons/playwright.svg',
+  ];
+
+  tools: string[] = [
+    '../../../assets/icons/visual-studio-code.svg',
+    '../../../assets/icons/figma.svg',
+    '../../../assets/icons/github.svg',
   ];
 }
