1d5df24e822ee9db1e75395eb9a7e2ec596a933d
Lexsak <bartoszpiprek123@gmail.com>
2023-12-16 09:51:37 +0100

    animation works

--- src/app/fade-out.directive.ts
+++ src/app/fade-out.directive.ts
@@ -1,10 +1,30 @@
-import { Directive } from '@angular/core';
+import {
+  Directive,
+  Input,
+  ElementRef,
+  Renderer2,
+} from '@angular/core';
 
 @Directive({
-  selector: '[appFadeOut]'
+  selector: '[appFadeOut]',
 })
 export class FadeOutDirective {
+  @Input() delay: number = 1000; // 10 sekund opóźnienia (możesz dostosować wartość)
 
-  constructor() { }
+  constructor(private el: ElementRef, private renderer: Renderer2) {}
 
+  ngAfterViewInit() {
+    setTimeout(() => {
+      this.fadeOut();
+    }, this.delay);
+  }
+
+  private fadeOut() {
+    this.renderer.setStyle(
+      this.el.nativeElement,
+      'transition',
+      'opacity 1s ease-in-out'
+    );
+    this.renderer.setStyle(this.el.nativeElement, 'opacity', '0');
+  }
 }
